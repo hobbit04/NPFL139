e20768fe91ac79b86c213e67074d513ab48ba161
hobbit04 <prlmejake9@gmail.com>
2026-04-18 17:29:02 +0200

    -100 -> 0 when crashed

--- labs/07/walker.py
+++ labs/07/walker.py
@@ -310,8 +310,7 @@ def main(env: npfl139.EvaluationEnv, args: argparse.Namespace) -> None:
             action = agent.predict_sampled_actions(state)
 
             next_state, reward, terminated, truncated, _ = vector_env.step(action)
-            if reward == -100:
-                reward = 0
+            reward = np.where((reward <= -100) & terminated, 0.0, reward)            
             done = terminated | truncated
             replay_buffer.append_batch(Transition(state, action, reward, done, next_state))
             state = next_state
